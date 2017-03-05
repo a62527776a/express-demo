a46c9fa44f0ed655b4f46c20a98bc8178c4b9cbb
a13868300128a@126.com <a13868300128a@126.com>
2017-03-05 13:56:59 +0800

    111

--- src/middleware/https.js
+++ src/middleware/https.js
@@ -10,11 +10,6 @@ const httpsOptions = {
 const PORT = 8080;
 
 const initHttps = (app) => {
-  app.get('/', (req, res) => {
-    res.sendFile(
-      path.join(__dirname, '../../static/https/index.html')
-    )
-  })
   const server = https.createServer(httpsOptions, app);
   server.listen(PORT, () => {
     console.log(`server start on ${PORT}`);
