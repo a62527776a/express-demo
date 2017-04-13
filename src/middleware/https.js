a460af1c0988ce36a87855f1f87509be3b26db43
a13868300128a@126.com <a13868300128a@126.com>
2017-04-13 19:31:12 +0800

    Signed-off-by: a13868300128a@126.com <a13868300128a@126.com>

--- src/middleware/https.js
+++ src/middleware/https.js
@@ -4,6 +4,8 @@ import https from 'https';
 import net from 'net';
 import crypto from 'crypto';
 
+const wsGUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+
 const httpsOptions = {
   key: fs.readFileSync(path.join(__dirname, '../../static/https/https.pem')),
   cert: fs.readFileSync(path.join(__dirname, '../../static/https/https.crt.crt'))
@@ -15,9 +17,25 @@ const TCPPORT = 8124;
 const initHttps = (app) => {
   const server = https.createServer(httpsOptions, app);
   const tcpServer = net.createServer((socket) => {
+    console.info('tcp client connected');
     socket.on('data', (data) => {
+    let dataString = data.toString(),
+      key = getWebSocketKey(dataString),
+      acceptKey;
+      if (key) {
+        console.info(key);
+        acceptKey = genAcceptKey(key);
+        socket.write('HTTP/1.1 101 Switching Protocols\r\n');
+        socket.write('Upgrade: websocket\r\n');
+        socket.write('Connection: Upgrade\r\n');
+        socket.write('Sec-WebSocket-Accept: ' + acceptKey + '\r\n');
+        socket.write('\r\n');
+      }
       // /Sec\-WebSocket\-Key:\s(.+)\r\n/
-    })
+    });
+    socket.on('end', function() {
+      console.info('client disconnected');
+    });
   });
   tcpServer.listen();
   server.listen(PORT, () => {
@@ -25,7 +43,7 @@ const initHttps = (app) => {
   });
   tcpServer.listen(TCPPORT, () => {
     console.log(`tcpServer start on ${TCPPORT}`);
-  })
+  });
 };
 
 export default initHttps;
